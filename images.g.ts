1a1a99ffed978190a7c5392363de70ce47ae3493
Franky5317U <155317227+Franky5317U@users.noreply.github.com>
2023-12-31 17:15:55 +0000

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -183,6 +183,25 @@ f f f c 3 c c c c c c f . .
 . . f f f f f f f f f f . . 
 . . . f f . . . f f f . . . 
 `;
+            case "image5":
+            case "myImage":return img`
+. . . c c c c c c . . . . . . . 
+. . c 6 7 7 7 7 6 c . . . . . . 
+. c 7 7 7 7 7 7 7 7 c . . . . . 
+c 6 7 7 7 7 7 7 7 7 6 c . . . . 
+c 7 c 6 6 6 6 c 7 7 7 c . . . . 
+f 7 6 f 6 6 f 6 7 7 7 f . . . . 
+f 7 7 7 7 7 7 7 7 7 7 f . . . . 
+. f 7 7 7 7 6 c 7 7 6 f . . . . 
+. . f c c c c 7 7 6 f c c c . . 
+. . c 6 2 7 7 7 f c c 7 7 7 c . 
+. c 6 7 7 2 7 7 c f 6 7 7 7 7 c 
+. c 1 1 1 1 7 6 6 c 6 6 6 c c c 
+. c 1 1 1 1 1 6 6 6 6 6 6 c . . 
+. c 6 1 1 1 1 1 6 6 6 6 6 c . . 
+. . c 6 1 1 1 1 1 7 6 6 c c . . 
+. . . c c c c c c c c c c . . . 
+`;
         }
         return null;
     })
